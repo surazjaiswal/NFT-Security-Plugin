10d7b7202aa26f122b7e1c5fd5070c4e976bc017
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-12-06 16:52:06 +0530

    Update bg.js

--- extension/bg.js
+++ extension/bg.js
@@ -4,33 +4,33 @@ var count = 0;
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    console.log(details);
-    {
-      count++;
+    console.log(details.url);
+    
+    // count++;
 
-      const url = details.url;
-      const domain = new URL(url).hostname.replace("www.", "");
+    // const url = details.url;
+    // const domain = new URL(url).hostname.replace("www.", "");
 
-      var table = document.getElementById("linkTable");
+    // var table = document.getElementById("linkTable");
 
-      //Add the data row.
-      var row = table.insertRow(-1);
+    // //Add the data row.
+    // var row = table.insertRow(-1);
 
-      //Add the data cells.
-      var cell = row.insertCell(-1);
-      cell.innerHTML = count;
+    // //Add the data cells.
+    // var cell = row.insertCell(-1);
+    // cell.innerHTML = count;
 
-      cell = row.insertCell(-1);
-      cell.innerHTML = domain;
+    // cell = row.insertCell(-1);
+    // cell.innerHTML = domain;
 
-      // cell = row.insertCell(-1);
-      // cell.innerHTML = details.type;
+    // // cell = row.insertCell(-1);
+    // // cell.innerHTML = details.type;
 
-      cell = row.insertCell(-1);
-      // cell.innerHTML = details.brand;
-      cell.innerHTML =
-        '<a href="' + details.url + '" target="_blank">' + "URL" + "</a>";
-    }
+    // cell = row.insertCell(-1);
+    // // cell.innerHTML = details.brand;
+    // cell.innerHTML =
+    //   '<a href="' + details.url + '" target="_blank">' + "URL" + "</a>";
+
     if (block_url.includes(details.url)) {
       return { cancel: true };
     } else {
